33b4bfe6f6f03c40c082c34aef7106487b1022c4
Chris Myers <cmyers78@gmail.com>
2016-07-14 16:33:33 -0600

    camera app working.  On to the collage screen.

--- Birthday Collage/Birthday Collage/CameraViewController.swift
+++ Birthday Collage/Birthday Collage/CameraViewController.swift
@@ -12,6 +12,10 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
     
     var receivedImage : UIImage?
     
+    @IBOutlet weak var clickContinueButtonOutlet: UIButton!
+    
+    // Creating a UIImagePickerController Instance
+    
     let chooseImage = UIImagePickerController()
     
     @IBOutlet weak var cameraImageView: UIImageView!
@@ -21,6 +25,12 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
         self.chooseImage.delegate = self
     }
 
+    @IBAction func continueButtonTapped(sender: UIButton) {
+        
+        
+        
+        
+    }
     
     @IBAction func cameraButtonTapped(sender: UIButton) {
         
@@ -34,5 +44,18 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
         
         self.chooseImage.allowsEditing = true
         self.presentViewController(self.chooseImage, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.cameraImageView.contentMode = .ScaleAspectFill
+            self.cameraImageView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        self.clickContinueButtonOutlet.hidden = false
+        
     }
 }
---
+++ Birthday Collage/Birthday Collage/CollageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CollageViewController.swift
+//  Birthday Collage
+//
+//  Created by Christopher Myers on 7/14/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CollageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
