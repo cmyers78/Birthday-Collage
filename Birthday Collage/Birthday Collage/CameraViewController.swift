167aeb1cf7f6a372ad7ee4b43f951a1ffd63acb3
Chris Myers <cmyers78@gmail.com>
2016-07-14 17:00:55 -0600

    final commit for the day.  heading home.

--- Birthday Collage/Birthday Collage/CameraViewController.swift
+++ Birthday Collage/Birthday Collage/CameraViewController.swift
@@ -22,6 +22,7 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.chooseImage.delegate = self
     }
 
